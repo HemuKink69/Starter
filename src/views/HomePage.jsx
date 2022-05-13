7cf690daf704fc907bdc4fabff20c78f649eaa97
Hemendra Singh <hemendrasingh.s2010@gmail.com>
2022-05-13 15:51:48 +0530

    first commit

---
+++ src/views/HomePage.jsx
@@ -0,0 +1,11 @@
+import { NavBar } from "../component/NavBar";
+import { Front } from "../component/Front";
+
+export function HomePage() {
+  return (
+    <>
+      <NavBar />
+      <Front />
+    </>
+  );
+}
